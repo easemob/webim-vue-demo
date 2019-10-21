bcc0c1d43ecb8e0ad506429c4d0893a44e661f8b
QWK-001 <QWK_001@163.com>
2019-10-21 12:17:23 +0800

    fix-消息框大量文本只显示一行

--- src/components/chat/message.vue
+++ src/components/chat/message.vue
@@ -4,7 +4,7 @@
         <!-- <div>{{type}}</div> -->
         <div>
             <a-icon type="left" class="user-goback" v-show="broken" @click="showUserList"/>
-          <span>{{`${activedKey[type].name } &nbsp;(${activedKey[type].groupid})`}}</span>
+          <span>{{`${activedKey[type].name } &nbsp;&nbsp; ${activedKey[type].groupid || ''}`}}</span>
           <a-icon v-if="type=='group'" type="ellipsis" class="user-ellipsis" @click="changeMenus"/>
           <a-dropdown v-else-if="type=='contact'">
             <a class="ant-dropdown-link user-ellipsis" href="#" @click="changeMenus">
