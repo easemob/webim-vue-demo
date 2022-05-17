2bc2a5f52d22a4426e8db5e1cda894aa14a5227e
逯雷雨 <444247905@qq.com>
2022-05-17 14:13:48 +0800

    fix: fix login bug

--- src/utils/WebIM.js
+++ src/utils/WebIM.js
@@ -1,8 +1,8 @@
 import config from './WebIMConfig';
 // import websdk from "easemob-websdk";
 
-// import websdk from 'websdk';
-import websdk from '../../static/Easemob-chat';
+import websdk from 'websdk';
+// import websdk from '../../static/Easemob-chat';
 
 import _ from 'lodash';
 import emedia from 'easemob-emedia';
