a2fedf54d7a8ffe13d6596d121d5bcc13c0892c2
逯雷雨 <444247905@qq.com>
2022-05-17 16:07:45 +0800

    feat: fix build package not run

--- src/utils/WebIM.js
+++ src/utils/WebIM.js
@@ -1,7 +1,7 @@
-import config from './WebIMConfig';
-// import websdk from "easemob-websdk";
-
-import websdk from 'websdk';
+import config from './WebIMConfig'
+import websdk from 'easemob-websdk'
+console.log(websdk, 'websdk')
+// import websdk from 'websdk';
 // import websdk from '../../static/Easemob-chat';
 
 import _ from 'lodash';
