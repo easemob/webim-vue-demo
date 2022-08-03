22db6c4ee45eaa30081a2ee3214b76f2f810c6ba
黄飞鹏 <1094521084@qq.com>
2022-08-03 11:19:47 +0800

    IM

--- src/IM/initwebsdk.js
+++ src/IM/initwebsdk.js
@@ -16,6 +16,7 @@ window.EaseIM = EaseIM = Easemob_SDK;
 /*
  * isHttpDNS： isPrivate为true开启私有化配置则走自有配置的url以及apiUrl，
  * 否则为true就SDK自助获取DNS地址。
+ * 【特别注意】如果不需要私有化配置，也就是自己定义url以及apiUrl。isHttpDNS、url、apiUrl，均可不用填写只用填入appKey！SDK内部会进行自动获取！
  **/
 EaseIM.conn = new Easemob_SDK.connection({
   appKey: CUSTOM_CONFIG.appKey ? CUSTOM_CONFIG.appKey : DEFAULT_APPKEY,
