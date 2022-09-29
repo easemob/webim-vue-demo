9d480ea03d468f79c18b081219324c80ee3c32c2
黄飞鹏 <1094521084@qq.com>
2022-09-29 18:10:20 +0800

    fix:刷新浏览器后播放铃声失败,增加权限申请弹窗.

--- src/hooks/usePlayRing.js
+++ src/hooks/usePlayRing.js
@@ -1,4 +1,5 @@
 import { watchEffect } from 'vue';
+import { ElMessageBox } from 'element-plus'
 import { useLocalStorage } from '@vueuse/core'
 import _ from 'lodash';
 export default function () {
@@ -18,8 +19,19 @@ export default function () {
     const playRing = _.throttle(() => {
         const ringDom = document.querySelector('#ring');
         console.log('>>>>铃声播放执行');
-        ringDom.play()
+        ringDom.play().then(res => console.log('>>>>>成功播放')).catch(() => openRing())
     }, 1000)
+
+    //请求播放权限
+    const openRing = () => {
+        ElMessageBox.alert('由于浏览器策略限制，需确认后方可播放新消息提示音！', {
+            confirmButtonText: 'OK',
+            callback: () => {
+                clickRing()
+            },
+        })
+    }
+
     return {
         isOpenPlayRing,
         clickRing,
