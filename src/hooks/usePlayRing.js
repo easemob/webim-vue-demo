8420104f959b063a1f2c2e59f04769b96b3501a3
黄飞鹏 <1094521084@qq.com>
2022-11-25 12:02:53 +0800

    fix:开启播放铃声未与用户，交互收到消息会重复触发alert窗问题。

--- src/hooks/usePlayRing.js
+++ src/hooks/usePlayRing.js
@@ -1,13 +1,11 @@
-import { watchEffect } from 'vue'
+import { ref } from 'vue'
 import { ElMessageBox } from 'element-plus'
 import { useLocalStorage } from '@vueuse/core'
 import _ from 'lodash'
+const is_need_open_ring = ref(false)
 export default function () {
     console.log('>>>>>>>此方法加载')
     const isOpenPlayRing = useLocalStorage('is_open_play_ring', true)
-    watchEffect(isOpenPlayRing, () => {
-        console.log('>>>>>>>监听铃声状态')
-    })
     //必须在播放铃声之前保证有一次交互
     //触发交互
     const clickRing = () => {
@@ -16,17 +14,22 @@ export default function () {
         ringDom.pause()
     }
     //播放铃声
-    const playRing = _.throttle(() => {
+    const playRing = _.throttle(async () => {
         const ringDom = document.querySelector('#ring')
         console.log('>>>>铃声播放执行')
-        ringDom
-            .play()
-            .then(() => console.log('>>>>>成功播放'))
-            .catch(() => openRing())
-    }, 1000)
+        try {
+            await ringDom.play()
+        } catch (error) {
+            console.log('铃声播放error', error)
+            if (!is_need_open_ring.value) {
+                openRing()
+            }
+        }
+    }, 3000)
 
     //请求播放权限
     const openRing = () => {
+        is_need_open_ring.value = true
         ElMessageBox.alert('由于浏览器策略限制，需确认后方可播放新消息提示音！', {
             confirmButtonText: 'OK',
             callback: () => {
