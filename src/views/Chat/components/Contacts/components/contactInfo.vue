be90e4939ef868e564fef27f754099a94b3f5cad
黄飞鹏 <1094521084@qq.com>
2023-02-23 17:52:13 +0800

    fix:removeUserFromBlocklist not function

--- src/views/Chat/components/Contacts/components/contactInfo.vue
+++ src/views/Chat/components/Contacts/components/contactInfo.vue
@@ -31,7 +31,9 @@ const blackStatus = ref(false)
 const switchStatus = ref(false)
 //判断单聊联系人是否在黑名单
 const isInBlackList = computed(() => {
-    const result = Array.from(store.state.Contacts.friendBlackList).includes(route.query.id)
+    const result = Array.from(store.state.Contacts.friendBlackList).includes(
+        route.query.id
+    )
     return result
 })
 //首次onMounted进行黑名单状态的初始赋值
@@ -39,19 +41,22 @@ onMounted(() => {
     blackStatus.value = isInBlackList.value
 })
 //监听route变化重新赋值switch状态
-watch(() => route.query.id, () => {
-    if (route.query.chatType === CHAT_TYPE.SINGLE) {
-        console.log('>>>>>监听变化赋值黑名单状态', isInBlackList.value)
-        blackStatus.value = isInBlackList.value
+watch(
+    () => route.query.id,
+    () => {
+        if (route.query.chatType === CHAT_TYPE.SINGLE) {
+            console.log('>>>>>监听变化赋值黑名单状态', isInBlackList.value)
+            blackStatus.value = isInBlackList.value
+        }
     }
-})
+)
 //执行加入或移出黑名单
 const changeBlackStatus = async () => {
     switchStatus.value = true
     if (blackStatus.value && route.query.id) {
         console.log('>>>>>移除黑明单')
         // 当前 removeUserFromBlackList 以及 addUsersToBlacklist 暂不支持promise 返回所以暂时获取不到其请求状态。
-        EaseChatClient.removeUserFromBlockList({
+        EaseChatClient.removeUserFromBlocklist({
             name: [route.query.id]
         })
         blackStatus.value = false
@@ -67,8 +72,6 @@ const changeBlackStatus = async () => {
     setTimeout(() => {
         store.dispatch('fetchBlackList')
     }, 500)
-
-
 }
 
 /* 单人删除好友 */
@@ -84,7 +87,8 @@ const delTheFriend = () => {
 const toChatMessage = () => {
     console.log('>>>>>>>...route.query')
     router.push({
-        path: '/chat/conversation/message', query: {
+        path: '/chat/conversation/message',
+        query: {
             id: route.query.id,
             chatType: route.query.chatType
         }
@@ -95,76 +99,107 @@ const toChatMessage = () => {
 <template>
     <div class="app_container">
         <el-header class="contactInfo_header">
-            <el-page-header style="margin-top: 12px;" :icon="ArrowLeft" @click="$router.back(-1)" />
+            <el-page-header
+                style="margin-top: 12px"
+                :icon="ArrowLeft"
+                @click="$router.back(-1)"
+            />
             <el-divider />
         </el-header>
         <el-main class="contactInfo_main">
             <div class="contactInfo_main_card">
                 <div class="contactInfo_box">
                     <div class="avatar">
-                        <el-avatar class="avatar_img" v-if="$route.query.chatType === CHAT_TYPE.SINGLE"
-                            :src="nowContactInfo.avatarurl ? nowContactInfo.avatarurl : defaultSingleAvatar">
+                        <el-avatar
+                            class="avatar_img"
+                            v-if="$route.query.chatType === CHAT_TYPE.SINGLE"
+                            :src="
+                                nowContactInfo.avatarurl
+                                    ? nowContactInfo.avatarurl
+                                    : defaultSingleAvatar
+                            "
+                        >
                         </el-avatar>
                         <!-- <UserStatus :userStatus="nowContactInfo.userStatus && nowContactInfo.userStatus" /> -->
-                        <el-avatar class="avatar_img" v-if="$route.query.chatType === CHAT_TYPE.GROUP"
-                            :src="defaultGroupAvatarUrl">
+                        <el-avatar
+                            class="avatar_img"
+                            v-if="$route.query.chatType === CHAT_TYPE.GROUP"
+                            :src="defaultGroupAvatarUrl"
+                        >
                         </el-avatar>
                     </div>
                     <div class="name">
                         <p v-if="$route.query.chatType === CHAT_TYPE.SINGLE">
-                            {{ nowContactInfo.nickname ? `${nowContactInfo.nickname}(${nowContactInfo.hxId})` :
-                                    nowContactInfo.hxId
-                            }}</p>
+                            {{
+                                nowContactInfo.nickname
+                                    ? `${nowContactInfo.nickname}(${nowContactInfo.hxId})`
+                                    : nowContactInfo.hxId
+                            }}
+                        </p>
                         <p v-if="$route.query.chatType === CHAT_TYPE.GROUP">
-                            {{ nowContactInfo.groupname ? `${nowContactInfo.groupname}(${nowContactInfo.groupid})` :
-                                    nowContactInfo.groupid
-                            }}</p>
+                            {{
+                                nowContactInfo.groupname
+                                    ? `${nowContactInfo.groupname}(${nowContactInfo.groupid})`
+                                    : nowContactInfo.groupid
+                            }}
+                        </p>
                     </div>
                     <div class="func_box">
-                        <div class="single_func" v-if="$route.query.chatType === CHAT_TYPE.SINGLE">
+                        <div
+                            class="single_func"
+                            v-if="$route.query.chatType === CHAT_TYPE.SINGLE"
+                        >
                             <div class="add_black_list">
                                 <p>加入黑名单</p>
-                                <el-switch v-model="blackStatus" :loading="switchStatus"
-                                    :before-change="changeBlackStatus" />
+                                <el-switch
+                                    v-model="blackStatus"
+                                    :loading="switchStatus"
+                                    :before-change="changeBlackStatus"
+                                />
                             </div>
                             <el-divider />
                             <div class="del_friend">
-                                <el-popconfirm title="确认删除此好友?" @confirm="delTheFriend">
+                                <el-popconfirm
+                                    title="确认删除此好友?"
+                                    @confirm="delTheFriend"
+                                >
                                     <template #reference>
                                         <span>删除好友</span>
                                     </template>
                                 </el-popconfirm>
-
                             </div>
                         </div>
                     </div>
-
                 </div>
                 <div class="contaactInfo_btn">
-                    <el-button v-if="$route.query.chatType === CHAT_TYPE.SINGLE" type="primary" size="large"
-                        @click="toChatMessage">发起会话
+                    <el-button
+                        v-if="$route.query.chatType === CHAT_TYPE.SINGLE"
+                        type="primary"
+                        size="large"
+                        @click="toChatMessage"
+                        >发起会话
                     </el-button>
-                    <el-button v-if="$route.query.chatType === CHAT_TYPE.GROUP" type="primary" size="large"
-                        @click="toChatMessage">进入群聊
+                    <el-button
+                        v-if="$route.query.chatType === CHAT_TYPE.GROUP"
+                        type="primary"
+                        size="large"
+                        @click="toChatMessage"
+                        >进入群聊
                     </el-button>
                     <!-- todo 待调整为新的获取群组列表接口，直接可以获取当前登陆id所在群组的权限然后添加上该功能 -->
                     <!--   <el-button v-if="$route.query.chatType === CHAT_TYPE.GROUP" type="danger" size="large">解散群组
-                    </el-button>
-                    <el-button v-if="$route.query.chatType === CHAT_TYPE.GROUP" type="danger" size="large">退出群组
-                    </el-button> -->
+                        </el-button>
+                        <el-button v-if="$route.query.chatType === CHAT_TYPE.GROUP" type="danger" size="large">退出群组
+                        </el-button> -->
                 </div>
             </div>
         </el-main>
     </div>
-
-
-
 </template>
-
 
 <style lang="scss" scoped>
 .app_container {
-    background: #F1F2F4;
+    background: #f1f2f4;
     height: 100%;
     border-radius: 0 5px 5px 0;
     overflow: hidden;
@@ -193,7 +228,6 @@ const toChatMessage = () => {
             &:hover {
                 background: #fff;
                 box-shadow: 12px 12px 2px 1px rgba(125, 125, 126, 0.068);
-
             }
 
             .contactInfo_box {
@@ -204,7 +238,7 @@ const toChatMessage = () => {
                 justify-content: flex-start;
                 align-items: center;
 
-                .avatar>.avatar_img {
+                .avatar > .avatar_img {
                     width: 80px;
                     height: 80px;
                 }
@@ -238,11 +272,9 @@ const toChatMessage = () => {
                         }
                     }
                 }
-
             }
 
             .contaactInfo_btn {
-
                 width: 80%;
                 text-align: center;
             }
