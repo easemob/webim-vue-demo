03a3bbaa2a6eed16c3b58dff16707b4e17076be7
黄飞鹏 <1094521084@qq.com>
2023-06-15 15:49:26 +0800

    prettier:contacts prettier

--- src/store/modules/contacts.js
+++ src/store/modules/contacts.js
@@ -4,13 +4,13 @@ import { sortPinyinFriendItem, handlePresence } from '@/utils/handleSomeData'
 import _ from 'lodash'
 const Contacts = {
     state: {
-    // friendList: useLocalStorage('friendList', {}),
+        // friendList: useLocalStorage('friendList', {}),
         friendList: {},
         // groupList: useLocalStorage('groupList', {}),
         groupList: {},
         // sortedFriendList: useLocalStorage('sortedFriendList', {}),
         sortedFriendList: {},
-        friendBlackList: [],
+        friendBlackList: []
     },
     mutations: {
         SET_FRIEND_LIST: (state, payload) => {
@@ -22,12 +22,12 @@ const Contacts = {
         SET_FRIEND_PRESENCE: (state, status) => {
             const friendList = state.friendList
             status.length > 0 &&
-        status.forEach((item) => {
-            const commonStatus = handlePresence(item)
-            if (friendList[commonStatus.uid]) {
-                friendList[commonStatus.uid].userStatus = commonStatus
-            }
-        })
+                status.forEach((item) => {
+                    const commonStatus = handlePresence(item)
+                    if (friendList[commonStatus.uid]) {
+                        friendList[commonStatus.uid].userStatus = commonStatus
+                    }
+                })
         },
         SET_SORDED_FRIEND_LIST: (state, payload) => {
             state.sortedFriendList = _.assign({}, payload)
@@ -47,7 +47,7 @@ const Contacts = {
                         groupid: id,
                         groupname: name,
                         disabled: disabled,
-                        groupDetail: data,
+                        groupDetail: data
                     }
                 }
                 console.log('>>>>>补充群详情', data)
@@ -58,34 +58,44 @@ const Contacts = {
             console.log('>>>>>>开始修改', payload)
             const { groupId, type, params } = payload
             //key(群id)，type（群详情对应要修改的字段）
-            if (state.groupList[groupId] && state.groupList[groupId].groupDetail) {
+            if (
+                state.groupList[groupId] &&
+                state.groupList[groupId].groupDetail
+            ) {
                 switch (type) {
-                //修改群名
-                case 'groupName':
-                    {
-                        console.log('>>>>>>进入群组名称修改')
-                        state.groupList[groupId].groupDetail.name = params
-                    }
-                    break
-                case 'groupDescription':
-                    {
-                        state.groupList[groupId].groupDetail.description = params
-                    }
-                    break
-                case 'addAffiliationsCount':
-                    {
-                        state.groupList[groupId].groupDetail.affiliations_count =
-                state.groupList[groupId].groupDetail.affiliations_count + 1
-                    }
-                    break
-                case 'delAffiliationsCount':
-                    {
-                        state.groupList[groupId].groupDetail.affiliations_count =
-                state.groupList[groupId].groupDetail.affiliations_count - 1
-                    }
-                    break
-                default:
-                    break
+                    //修改群名
+                    case 'groupName':
+                        {
+                            console.log('>>>>>>进入群组名称修改')
+                            state.groupList[groupId].groupDetail.name = params
+                        }
+                        break
+                    case 'groupDescription':
+                        {
+                            state.groupList[groupId].groupDetail.description =
+                                params
+                        }
+                        break
+                    case 'addAffiliationsCount':
+                        {
+                            state.groupList[
+                                groupId
+                            ].groupDetail.affiliations_count =
+                                state.groupList[groupId].groupDetail
+                                    .affiliations_count + 1
+                        }
+                        break
+                    case 'delAffiliationsCount':
+                        {
+                            state.groupList[
+                                groupId
+                            ].groupDetail.affiliations_count =
+                                state.groupList[groupId].groupDetail
+                                    .affiliations_count - 1
+                        }
+                        break
+                    default:
+                        break
                 }
             }
         },
@@ -100,21 +110,27 @@ const Contacts = {
                 console.log('>>>>>从本地群组列表中删除某个群')
                 state.groupList[groupId] && delete state.groupList[groupId]
             }
-        },
+        }
     },
     actions: {
-    //获取好友列表
+        //获取好友列表
         fetchFriendList: async ({ dispatch, commit }) => {
             const friendListData = {}
             try {
                 //获取好友列表
                 const { data } = await EaseChatClient.getContacts()
                 data.length > 0 &&
-          data.map((item) => (friendListData[item] = { hxId: item }))
+                    data.map((item) => (friendListData[item] = { hxId: item }))
                 //获取好友列表对应的用户属性
-                const friendListWithInfos = await dispatch('getOtherUserInfo', data)
+                const friendListWithInfos = await dispatch(
+                    'getOtherUserInfo',
+                    data
+                )
                 //合并两对象
-                const mergedFriendList = _.merge(friendListData, friendListWithInfos)
+                const mergedFriendList = _.merge(
+                    friendListData,
+                    friendListWithInfos
+                )
                 //合并后的好友列表数据进行排序并单独提交处理
                 const sortFriendList = sortPinyinFriendItem(mergedFriendList)
                 commit('SET_SORDED_FRIEND_LIST', sortFriendList)
@@ -137,8 +153,8 @@ const Contacts = {
         //获取他人用户属性
         getOtherUserInfo: async ({ commit }, users) => {
             /**
-       * @param {String|Array} users - 用户id
-       */
+             * @param {String|Array} users - 用户id
+             */
 
             return new Promise(async (resolve, reject) => {
                 let usersInfosObj = {}
@@ -146,15 +162,21 @@ const Contacts = {
                 const usersArr = _.chunk([...users], 99) //分拆users 用户属性获取一次不能超过100个
                 try {
                     usersArr.length > 0 &&
-            usersArr.map((userItem) =>
-                requestTask.push(EaseChatClient.fetchUserInfoById(userItem))
-            )
+                        usersArr.map((userItem) =>
+                            requestTask.push(
+                                EaseChatClient.fetchUserInfoById(userItem)
+                            )
+                        )
                     const result = await Promise.all(requestTask)
                     const usersInfos = _.map(result, 'data')
                     usersInfos.length > 0 &&
-            usersInfos.map(
-                (item) => (usersInfosObj = Object.assign(usersInfosObj, item))
-            )
+                        usersInfos.map(
+                            (item) =>
+                                (usersInfosObj = Object.assign(
+                                    usersInfosObj,
+                                    item
+                                ))
+                        )
                     resolve(usersInfosObj)
                 } catch (error) {
                     reject(error)
@@ -167,19 +189,21 @@ const Contacts = {
             const usersArr = _.chunk([...users], 5) //分拆users 订阅好友状态一次不能超过100个
             try {
                 usersArr.length > 0 &&
-          usersArr.map((userItem) =>
-              requestTask.push(
-                  EaseChatClient.subscribePresence({
-                      usernames: userItem,
-                      expiry: 30 * 24 * 3600,
-                  })
-              )
-          )
+                    usersArr.map((userItem) =>
+                        requestTask.push(
+                            EaseChatClient.subscribePresence({
+                                usernames: userItem,
+                                expiry: 30 * 24 * 3600
+                            })
+                        )
+                    )
                 const resultData = await Promise.all(requestTask)
-                const usersPresenceList = _.flattenDeep(_.map(resultData, 'result')) //返回值是个二维数组，flattenDeep处理为一维数组
+                const usersPresenceList = _.flattenDeep(
+                    _.map(resultData, 'result')
+                ) //返回值是个二维数组，flattenDeep处理为一维数组
                 const tobeCommitRes =
-          usersPresenceList.length > 0 &&
-          usersPresenceList.filter((p) => p.uid !== '')
+                    usersPresenceList.length > 0 &&
+                    usersPresenceList.filter((p) => p.uid !== '')
                 console.log('resultData', resultData)
                 commit('SET_FRIEND_PRESENCE', tobeCommitRes)
             } catch (error) {
@@ -189,7 +213,7 @@ const Contacts = {
         //取消订阅
         unsubFriendsPresence: async ({ commit }, user) => {
             const option = {
-                usernames: [...user],
+                usernames: [...user]
             }
             EaseChatClient.unsubscribePresence(option).then((res) => {
                 console.log('>>>>>>>成功取消订阅', res)
@@ -200,7 +224,7 @@ const Contacts = {
             const res = await EaseChatClient.getJoinedGroups({
                 // needAffiliations: true,
                 // needRole: true,
-                ...params,
+                ...params
             })
             const goupListData = _.keyBy(res.data, 'groupid')
             commit('SET_GROUP_LIST', { setType: 'init', data: goupListData })
@@ -209,17 +233,17 @@ const Contacts = {
         //获取指定群详情
         getAssignGroupDetail: async ({ dispatch, commit }, goupsId) => {
             const options = {
-                groupId: goupsId, // 群组id
+                groupId: goupsId // 群组id
             }
             const result = await EaseChatClient.getGroupInfo(options)
             // console.log('>>>>>>>群详情获取成功result', result);
             result.data &&
-        commit('SET_GROUP_LIST', {
-            setType: 'replenish',
-            data: result.data[0],
-        })
-        },
-    },
+                commit('SET_GROUP_LIST', {
+                    setType: 'replenish',
+                    data: result.data[0]
+                })
+        }
+    }
 }
 
 export default Contacts
