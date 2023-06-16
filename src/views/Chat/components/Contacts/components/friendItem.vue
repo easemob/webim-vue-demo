758529ede5082112c8a3330cd0e76f966b74c72a
黄飞鹏 <1094521084@qq.com>
2023-06-16 14:18:17 +0800

    opt:优化好友增减好友列表拉取以及订阅请求,减少不必要的全量请求.

--- src/views/Chat/components/Contacts/components/friendItem.vue
+++ src/views/Chat/components/Contacts/components/friendItem.vue
@@ -9,42 +9,60 @@ import defaultAvatar from '@/assets/images/avatar/theme2x.png'
 const store = useStore()
 
 //处理friendList进行分类处理
-const classifyFriendList = computed(() => store.state.Contacts.sortedFriendList)
+const classifyFriendList = computed(() => store.getters.sortedFriendList)
 //点击对应联系人跳转至用户详情页
 const { CHAT_TYPE } = messageType
 </script>
 
 <template>
     <div class="friendItem_container">
-        <div v-for="(friendName, friendItemKey) in  classifyFriendList" :key="friendItemKey">
+        <div
+            v-for="(friendName, friendItemKey) in classifyFriendList"
+            :key="friendItemKey"
+        >
             <div class="friend_main">
-
                 <div class="friend_title">
                     <p>
-                        {{ friendItemKey === ' ' ? '#' : friendItemKey.toUpperCase() }}
+                        {{
+                            friendItemKey === ' '
+                                ? '#'
+                                : friendItemKey.toUpperCase()
+                        }}
                     </p>
 
-                    <el-divider style="margin:0;" />
+                    <el-divider style="margin: 0" />
                 </div>
 
                 <el-row>
-                    <el-col class="friendItem_box" :span="24" v-for="item in friendName" :key="item.hxId"
-                        @click="$emit('toContacts', { id: item.hxId, chatType: CHAT_TYPE.SINGLE })">
-                        <el-avatar style="margin-right: 11px;" :size="33.03"
-                            :src="item.avatarurl ? item.avatarurl : defaultAvatar">
+                    <el-col
+                        class="friendItem_box"
+                        :span="24"
+                        v-for="item in friendName"
+                        :key="item.hxId"
+                        @click="
+                            $emit('toContacts', {
+                                id: item.hxId,
+                                chatType: CHAT_TYPE.SINGLE
+                            })
+                        "
+                    >
+                        <el-avatar
+                            style="margin-right: 11px"
+                            :size="33.03"
+                            :src="
+                                item.avatarurl ? item.avatarurl : defaultAvatar
+                            "
+                        >
                         </el-avatar>
                         <span class="friend_name">
                             {{ item.nickname ? item.nickname : item.hxId }}
                         </span>
-
                     </el-col>
                 </el-row>
             </div>
         </div>
     </div>
 </template>
-
-
 
 <style lang="scss" scoped>
 .friendItem_box {
@@ -56,7 +74,7 @@ const { CHAT_TYPE } = messageType
     justify-content: flex-start;
     // background: #000;
     padding: 0 22px;
-    background: #EFEFEF;
+    background: #efefef;
     font-weight: 500;
     font-size: 14px;
     line-height: 20px;
@@ -75,7 +93,7 @@ const { CHAT_TYPE } = messageType
     }
 
     &:hover {
-        background: #DCDCDC;
+        background: #dcdcdc;
     }
 }
 
